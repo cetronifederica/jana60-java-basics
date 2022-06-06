68d52bfe2b6386e93e13e349aaa4ba0261a0c989
federicacetroni <cetronifederica97@gmail.com>
2022-06-06 11:37:03 +0200

    second commit

---
+++ src/jana60/BookStore.java
@@ -0,0 +1,46 @@
+package jana60;
+
+public class BookStore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// titolo
+		String titolo = "9 algoritmi che hanno cambiato il futuro";
+		// formato
+		boolean copertinaFlessibile = true;
+		boolean formatoKindle = false;
+		// data di pubblicazione
+		short giornoPubblicazione = 1;
+		short mesePubblicazione = 11; 
+		int annoPubblicazione = 2012;
+		
+		// autore
+		String autore = "john McCormick";
+		
+		// rating
+		float rating = 4.3F;
+		int numeroVoti = 23;
+		
+		// descrizione
+		String descrizione = "Quotisiamente, senza pensarci troppo, con i nostri strumenti tecnologici";
+		
+		// prezzo
+		float prezzoFinale = 17.10f;
+		float prezzoConsigliato = 18.00f;
+		double sconto = 0.05;
+		
+		// editore 
+		String editore = "Apogeo Education"; 
+		
+		// isbn-10
+		String isbn = "8838787999";
+		
+		// dimensioni
+		float altezza = 21f; 
+		float profondita = 1.7f; 
+		float larghezza = 13.7f;
+		
+
+	}
+
+}
--- src/jana60/EserciziVariabili.java
+++ src/jana60/EserciziVariabili.java
@@ -3,7 +3,13 @@ package jana60;
 public class EserciziVariabili {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Esercizi con le variabili
+String saluto;
+saluto = "ciao";
+//assegno a saluto il valore arrivederci
+saluto = "arrivederci";
+System.out.println(saluto);
+
 
 	}
 
