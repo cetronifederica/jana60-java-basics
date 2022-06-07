ca6f7bb909104b0e25f7827d7ed90142cb326deb
federicacetroni <cetronifederica97@gmail.com>
2022-06-07 09:46:57 +0200

    livecoding

---
+++ src/jana60/PasswordGeneration.java
@@ -0,0 +1,21 @@
+package jana60;
+
+public class PasswordGeneration {
+
+	public static void main(String[] args) {
+		
+		// live coding 
+		
+		String nome = "Federica"; 
+		String cognome = "Cetroni";
+		String colorePreferito = "nero"; 
+		int giornoNascita = 3;
+		int meseNascita = 3;
+		int annoNascita = 1997; 
+		int  sommaDataNascita = giornoNascita + meseNascita + annoNascita;
+		String password = nome + cognome + colorePreferito + sommaDataNascita; 
+		System.out.println(password);
+
+	}
+
+}
