a611cb98234093c2fe2e6c4bd2959502d0fdef20
federicacetroni <cetronifederica97@gmail.com>
2022-06-07 11:25:35 +0200

    operazioni condizionali

---
+++ src/jana60/EserciziOperatoriRelazionali.java
@@ -0,0 +1,22 @@
+package jana60;
+
+public class EserciziOperatoriRelazionali {
+
+	public static void main(String[] args) {
+		// live coding
+		
+		int x = 3;
+		int y = 5;
+		
+		if(x==y) { 
+			System.out.println("e uguale a y");
+		} else if(x<y) {
+			System.out.println("x minore da y");
+		} else {
+			System.out.println("x maggiore di y");
+		}
+		
+
+	}
+
+}
